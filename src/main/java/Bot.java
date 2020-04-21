b341abf5e72ed036b9945f22b15d847bd55088a9
Oloir <swetozarka@yandex.ru>
2020-04-21 23:17:39 +0300

    !!!

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,16 +1,22 @@
+// import com.sun.org.apache.xpath.internal.operations.Quo;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Bot extends TelegramLongPollingBot{
@@ -42,17 +48,17 @@ public class Bot extends TelegramLongPollingBot{
     public void sendMsg(Message message, String text){
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
-//        sendMessage.enableMarkdownV2(true); 
+//        sendMessage.enableMarkdownV2(true);
         sendMessage.setChatId(message.getChatId().toString());
         sendMessage.setReplyToMessageId(message.getMessageId());
         sendMessage.setText(text);
-        try{
+       try{
 
-           setButtons(sendMessage);
-            execute(sendMessage);
+     //      setButtons(sendMessage);
+           execute(sendMessage);
 
-        }catch (TelegramApiException e){
-            e.printStackTrace();
+       }catch (TelegramApiException e){
+           e.printStackTrace();
         }
 
 
@@ -62,37 +68,116 @@ public class Bot extends TelegramLongPollingBot{
 
 
     public void onUpdateReceived(Update update) {
+        if(update.hasMessage()){
+            if(update.getMessage().hasText()){
+                if(update.getMessage().getText().equals("Hello")){
+                    try {
+                        execute(sendInlineKeyBoardMessage(update.getMessage().getChatId()));
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }else if(update.hasCallbackQuery()){
+            try {
+                execute(new SendMessage().setText(
+                        update.getCallbackQuery().getData())
+                        .setChatId(update.getCallbackQuery().getMessage().getChatId()));
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+        ArrayList<String> List1 = new ArrayList<String>();
         Message message = update.getMessage();
         if(message !=null && message.hasText()){
             switch (message.getText()){
-                case "Загрузить":
-                    sendMsg(message, "ХОЧУ СКАЗАТЬ ЭТОМУ ОБЭМЕ");
-                    break;
+//
+
                 default:
-                    sendMsg(message, "УУУУУ СУКА");
+                    FileWriter FW = null;
+                    try {
+                        FW = new FileWriter("C:/Users/Public/test.txt", true);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        FW.write("\n" + message.getText());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        FW.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        FW.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    List1.add(message.getText());
+
+                    break;
+
+
+            }
+
+
+
 
             }
         }
+
+
+
+
+
+
+    public static SendMessage sendInlineKeyBoardMessage(long chatId) {
+
+
+
+        Quote QuoM = new Quote();
+
+
+
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
+
+        inlineKeyboardButton1.setText("дай цитату!");
+        inlineKeyboardButton1.setCallbackData(QuoM.GetQuote());
+
+        List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
+
+        keyboardButtonsRow1.add(inlineKeyboardButton1);
+
+
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        rowList.add(keyboardButtonsRow1);
+        inlineKeyboardMarkup.setKeyboard(rowList);
+        return new SendMessage().setChatId(chatId).setText("Пример").setReplyMarkup(inlineKeyboardMarkup);
     }
 
 
-    public void setButtons(SendMessage sendMessage){
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-        sendMessage.setReplyMarkup(replyKeyboardMarkup);
-        replyKeyboardMarkup.setSelective(true);
-        replyKeyboardMarkup.setResizeKeyboard(true);
-        replyKeyboardMarkup.setOneTimeKeyboard(false);
 
-        List<KeyboardRow> keyboardRowsList = new ArrayList<>();
 
-        KeyboardRow firstRow = new KeyboardRow();
-
-        firstRow.add(new KeyboardButton("Загрузить"));
-        firstRow.add(new KeyboardButton("Выгрузить"));
-
-        keyboardRowsList.add(firstRow);
-        replyKeyboardMarkup.setKeyboard(keyboardRowsList);
-    }
+//        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+//        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+//        replyKeyboardMarkup.setSelective(true);
+//        replyKeyboardMarkup.setResizeKeyboard(true);
+//        replyKeyboardMarkup.setOneTimeKeyboard(false);
+//
+//        List<KeyboardRow> keyboardRowsList = new ArrayList<>();
+//
+//        KeyboardRow firstRow = new KeyboardRow();
+//
+//        firstRow.add(new KeyboardButton("Дай цитату!"));
+//
+//        keyboardRowsList.add(firstRow);
+//        replyKeyboardMarkup.setKeyboard(keyboardRowsList);
 
 
 
---
+++ src/main/java/Quote.java
@@ -0,0 +1,78 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Quote  {
+    ArrayList<String> List1 = new ArrayList<String>();
+    Scanner Scan;
+    public String Quote;
+
+
+
+
+    public void write() throws IOException {
+
+
+
+                FileWriter FW = new FileWriter("C:/Users/Public/test.txt", true);
+                FW.write("\n" + Quote);
+                FW.flush();
+                FW.close();
+
+
+        List1.add(Quote);
+    }
+
+
+
+
+
+
+
+
+    public String GetQuote() {
+
+
+
+
+        {
+            try {
+                Scan = new Scanner(new File("C:/Users/Public/test.txt"));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        {
+            while (Scan.hasNextLine()) {
+                List1.add(Scan.nextLine());
+            }
+            String[] array = List1.toArray(new String[0]);
+
+            double z = Math.random() * List1.size();
+            int x = (int) Math.round(z);
+            System.out.println(z);
+
+            return (List1.get(x));
+        }
+    }
+
+
+
+
+    public void Quote(String quote) {
+        Quote = quote;
+    }
+
+
+
+//    public void Add() {
+//        List1.add(Quote);
+//    }
+
+
+
+}
